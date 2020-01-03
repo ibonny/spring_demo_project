5926bdfcf1161a675d0b795c7fa36a854f9dae64
ibonny <ibonny@gmail.com>
2020-01-03 19:01:42 +0000

    Saving changes.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     $("#testParam").html("Loading...");
 
+    var value = $("#nameval").html();
+
+    console.log(`Name value is: ${value}`);
+
     $.get("/greeting", function(response) {
         console.log(response);
 
-        $("#testParam").html(`${response.id}: ${response.content}`)
+        $("#testParam").html(`${response.id}: ${response.content}`);
     });
 });
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -5,6 +5,10 @@
 
     <p id="testParam"></p>
 
+    <#--  This is a good way to take a variable from Spring Boot and inject it
+          into Javascript.  -->
+    <p style="display: none;" id="nameval">${name}</p>
+
     <script src='/webjars/jquery/3.4.1/jquery.js'></script>
     <script src="js/index.js"></script>
 </html>
